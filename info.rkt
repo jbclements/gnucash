d7bc8f4f3ab25c5c35339f2a6ec341f75352473e
John Clements <clements@racket-lang.org>
2015-03-20 13:38:14 -0700

    add deps

--- info.rkt
+++ info.rkt
@@ -5,4 +5,12 @@
 (define deps (list "base"
                    "sxml"
                    "srfi-lib"
-                   "srfi-lite-lib"))
+                   "srfi-lite-lib"
+                   "memoize"
+                   "rackunit-lib"
+                   "typed-racket-lib"
+                   "unstable-contract-lib"))
+
+(define build-deps
+  (list "racket-doc"
+        "scribble-lib"))
